02769afec8e81cbe287fe9906a8f5114beb2ac8c
Ryan Endacott <rzeg24@gmail.com>
2013-12-05 15:38:51 -0600

    working summarizer website!

--- server.py
+++ server.py
@@ -1,10 +1,18 @@
 import os
-from flask import Flask, render_template
-#from summary import summarize
+from flask import Flask, render_template, request
+from summary import summarize
 
 app = Flask(__name__)
 app.debug = True
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello():
+  if request.method == 'POST':
+    text = request.form.get('text', None)
+    if text is None:
+      return render_template('index.html', summary="Error, didn't get text to summarize.")
+    else:
+      s = summarize(request.form.get('text', None), raw=True)
+      return render_template('index.html', summary=s)
+
   return render_template('index.html')
